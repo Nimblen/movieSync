a2edc752b2f028b9713ff547e40a9ae2d6109c1f
Nimblen <nnurhodjaev@gmail.com>
2024-12-14 12:48:35 +0300

    feat || add serializers for room

---
+++ backend/src/apps/room/serializers/room.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from src.apps.core.constants import RoomTypes
+from src.apps.room.validators.room_validator import UniqueRoomValidator
+from src.apps.room.services.room_service import RoomService
+from src.apps.core.repositories.redis_repository import RedisRepository
+
+repository = RedisRepository()
+room_service = RoomService(repository)
+
+class RoomSerializer(serializers.Serializer):
+    room_id = serializers.CharField(max_length=255, validators=[UniqueRoomValidator(room_service)])
+    movie_id = serializers.CharField(max_length=255)
+    room_owner = serializers.CharField(max_length=255)
+    room_type = serializers.ChoiceField(choices=RoomTypes.ROOM_TYPE_CHOICES)
+    max_users = serializers.IntegerField(default=5, min_value=1)
+
