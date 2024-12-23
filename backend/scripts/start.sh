bed54be37f4aca25da7acc72daddaff9df6bfe60
Nimblen <nnurhodjaev@gmail.com>
2024-12-23 22:56:53 +0500

    feat || add docker and nginx

---
+++ backend/scripts/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+# Apply database migrations
+python manage.py makemigrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput --verbosity 0
+
+# Start Gunicorn and Daphne
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 &
+daphne -b 0.0.0.0 -p 8020 config.asgi:application &
+
+# Wait for all background processes
+wait
+
+
